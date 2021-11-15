60a0e1efc2d0c8a24e353fedb18b48e4d1338f34
Will Gresham <willgresham34@gmail.com>
2021-11-15 16:58:48 -0500

    service worker done now getting error for manifest json

---
+++ public/service-worker.js
@@ -0,0 +1,59 @@
+const { response } = require("express");
+
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+var cacheUrls = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Cache Opened");
+      return cache.addAll(cacheUrls);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  if (e.request.url.inlcludes("/api/")) {
+    e.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(e.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(e.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(e.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+
+  e.respondWith(
+    fetch(e.request).catch(function () {
+      return caches.match(e.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (e.request.header.get("accept").inlcludes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
